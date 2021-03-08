9084b05d11ca7f9bf32eb9324dda09df6c7ebaf0
leopold lacroix <leopold.lacroix@yahoo.fr>
2021-03-08 16:39:12 +0100

    un debut de visu

--- Morpion.js
+++ Morpion.js
@@ -2,22 +2,25 @@ class Morpion {
     constructor() {
         this.lines = 3;
         this.columns = 3;
-        this.content = generateHtml;
     }
 
     generateHtml() {
+        content = '';
         for (let line = 0; line < this.lines; line++) {
-            morpionContent += "<div class = line" + line + ">";
+            content += "<div class = line id = line" + line + ">";
 
             for (let column = 0; column < this.columns; column++) {
-                morpionContent +=
-                    "<div class = column" + column + ">" +
+                content +=
+                    "<div class = column id = column" + column + ">" +
                     line + column +
                     "</div>";
                 
             }
 
-            morpionContent += "</div >";
+            content += "</div >";
         }
+        document.getElementById('morpion').innerHTML = content;
     }
 }
+
+export { Morpion };
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-import Morpion from './Morpion.js';
+import { Morpion } from './Morpion.js';
 
-morpion = new Morpion();
+document.getElementById("addLine").onclick = addLine;
+document.getElementById("addColumn").onclick = addColumn;
+
+var morpion = new Morpion();
 
 let morpionComponent = document.getElementById('morpion');
 morpion.innerHTML = morpion.generateHtml();
+
+function addLine() {
+    morpion.lines += 1;
+    morpion.generateHtml();
+}
+
+function addColumn() {
+    morpion.columns += 1;
+    morpion.generateHtml();
+}
