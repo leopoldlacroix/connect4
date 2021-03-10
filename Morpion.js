61118fb50f53c777efecd066d2d8eed274b1af35
depotoire <leopold.lacroix@isep.fr>
2021-03-10 22:57:35 +0100

    plus qu'a faire l'ordi

--- Morpion.js
+++ Morpion.js
@@ -1,26 +1,70 @@
 class Morpion {
+    static players = ['X','0'];
     constructor() {
-        this.lines = 3;
-        this.columns = 3;
+        this.lines = 5;
+        this.columns = 5;
+        this.turn = 0;
     }
 
     generateHtml() {
         content = '';
         for (let line = 0; line < this.lines; line++) {
-            content += "<div class = line id = line" + line + ">";
+            content += `<div class = line id = line${line}">`;
 
             for (let column = 0; column < this.columns; column++) {
                 content +=
-                    "<div class = column id = column" + column + ">" +
-                    line + column +
-                    "</div>";
-                
+                    `<div class=field id=${"field"+line+column}>_</div>`;
             }
-
             content += "</div >";
         }
         document.getElementById('morpion').innerHTML = content;
+        
     }
+
+    action(){
+        this.turn = (this.turn+1) % Morpion.players.length;
+        return Morpion.players[this.turn];
+    }
+    
+    check(){
+        let state = '';
+        for (let i = 0; i < this.lines; i++) {
+            for (let j = 0; j < this.columns; j++) {
+                let fieldChangingColumn = document.getElementById("field"+i+j).innerHTML;
+                state += fieldChangingColumn;
+            }
+        }
+        let checks = this.allchecks();
+        for (let expression of checks) {
+            let regex = new RegExp(expression);
+            let match = state.match(regex);
+            if (match && match.length > 1) {
+                return match[1];
+            }
+        }
+    }
+
+    allchecks(){
+        let checks = [];
+        for (let representation of Morpion.players) {
+            checks.push(representation.repeat(4)); //regex for line win
+            checks.push(`(${representation}).{${this.columns-2}}`.repeat(3)+representation); //for sw diags
+            checks.push(`(${representation}).{${this.columns-1}}`.repeat(3)+representation); //for columns
+            checks.push(`(${representation}).{${this.columns}}`.repeat(3)+representation); //for se diags
+        }
+        return checks;
+    }
+
+    addLine() {
+        this.lines += 1;
+        this.generateHtml();
+    }
+
+    addColumn() {
+        this.columns += 1;
+        this.generateHtml();
+    }
+
 }
 
 export { Morpion };
--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 import { Morpion } from './Morpion.js';
 
-document.getElementById("addLine").onclick = addLine;
-document.getElementById("addColumn").onclick = addColumn;
 
 var morpion = new Morpion();
 
 let morpionComponent = document.getElementById('morpion');
-morpion.innerHTML = morpion.generateHtml();
+morpionComponent = morpion.generateHtml();
+
+document.getElementById("addLine").onclick = addLine;
+document.getElementById("addColumn").onclick = addColumn;
+document.getElementById("restart").onclick = restart;
+
 
 function addLine() {
-    morpion.lines += 1;
-    morpion.generateHtml();
+    morpion.addLine();
+    onclicks();
 }
 
 function addColumn() {
-    morpion.columns += 1;
+    morpion.addColumn();
+    onclicks();
+}
+
+function restart() {
     morpion.generateHtml();
+    onclicks();
+}
+
+onclicks();
+function onclicks(){
+    let fields = document.getElementsByClassName("field") 
+    for (let field of fields) {
+        field.onclick = action;   
+    }
+}
+
+function action(){
+    if(Morpion.players.includes(this.innerHTML)){
+        return;
+    }
+    this.innerHTML = morpion.action()
+    let checkRes = morpion.check();
+    if(checkRes){
+        document.querySelector("footer").innerHTML = `${checkRes} won!`;
+    }
 }
